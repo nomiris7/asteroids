8665c3997fe85191e2f1e0b0e9cdbad654fc547e
Szymon Kyrcz <nomiris7@gmail.com>
2025-02-13 19:07:12 +0100

    Uppdated: player can move and added groups

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
 
     while True:
@@ -14,10 +17,12 @@ def main():
             if event.type ==pygame.QUIT:
                 return
             
+        updatable.update(dt)
         screen.fill((0,0,0))
-        player.draw(screen=screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
-        
+
         dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
